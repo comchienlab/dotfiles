4d9b8d0eecfda46870f20c3834a7ef49ec3ce163
Ryan Phan <51282340+ryanphanrp@users.noreply.github.com>
2025-02-23 10:31:18 +0700

    update

--- qksetup.sh
+++ qksetup.sh
@@ -223,6 +223,15 @@ case $choice in
 
         gum style --foreground 196 "Disabling Tracker services..."
         systemctl --user mask tracker-extract-3 tracker-miner-fs-3 tracker-miner-control-3 tracker-miner-rss-3 tracker-writeback-3 tracker-xdg-portal-3
+        sudo apt-mark hold tracker
+        sudo apt-mark hold tracker-extract
+        sudo apt-mark hold tracker-miner-fs
+
+        sudo chmod -x /usr/libexec/tracker-extract-3
+        sudo chmod -x /usr/libexec/tracker-miner-fs-3
+
+        tracker3 reset --filesystem --rss # Clean all database
+        tracker3 daemon --terminate
         ;;
 
     "Install Ghostty Terminal")
